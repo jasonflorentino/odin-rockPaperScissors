7f048afd930c77156233920214ac00c1c36f178d
Jason2008 <72258481+jasonflorentino@users.noreply.github.com>
2020-11-18 14:57:28 -0500

    Added player selection and game result

--- game.js
+++ game.js
@@ -1,62 +1,74 @@
 // game.js
 // Main rock paper scissors game functions
 
-function computerPlay() {
-  let answers = ["rock", "paper", "scissors"];
+function computerChoice() {
+  let answers = ["Rock", "Paper", "Scissors"];
     return answers[Math.floor(Math.random()*answers.length)];
 };
 
 function playRound(playerSelection, computerSelection) {
-  console.log("You chose: ", playerSelection)
-  console.log("Computer chose: ", computerSelection)
 
   if(playerSelection == computerSelection) {
     return "It's a tie!";
   }
 
-  if(playerSelection == "rock" && computerSelection == "paper") {
-    return "You lose!";
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+  if(playerSelection == "Rock" && computerSelection == "Paper") {
+    return "You Lose!";
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
     return "You Win!";
   }
 
-  if(playerSelection == "paper" && computerSelection == "rock") {
+  if(playerSelection == "Paper" && computerSelection == "Rock") {
     return "You Win!";
-  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
     return "You Lose!";
   }
 
-  if(playerSelection == "scissors" && computerSelection == "rock") {
+  if(playerSelection == "Scissors" && computerSelection == "Rock") {
     return "You Lose!";
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
     return "You Win!";
   }
 
 };
 
-function clicked() {
-  
+// function clicked() {
+
+// }
+
+// function rockClick() {
+//   console.log("Rock");
+// }
+
+// function paperClick() {
+//   console.log("Paper");
+// }
+
+// function scissorsClick() {
+//   console.log("Scissors");
+// }
+
+// const rock = document.getElementById("rock");
+// const paper = document.getElementById("paper");
+// const scissors = document.getElementById("scissors");
+
+// rock.addEventListener("click", rockClick);
+// paper.addEventListener("click", paperClick);
+// scissors.addEventListener("click", scissorsClick);
+
+function clicked(e) {
+  const playerChoice = e.srcElement.textContent;
+  const compChoice = computerChoice();
+  const winMsg = playRound(playerChoice, compChoice);
+
+  winner.textContent = winMsg;
+  result.textContent = `You chose ${playerChoice}. Computer chose ${compChoice}.`;
 }
 
-function rockClick() {
-  console.log("Rock");
-}
-
-function paperClick() {
-  console.log("Paper");
-}
-
-function scissorsClick() {
-  console.log("Scissors");
-}
-
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-
-rock.addEventListener("click", rockClick);
-paper.addEventListener("click", paperClick);
-scissors.addEventListener("click", scissorsClick);
+const winner = document.getElementById("winner");
+const result = document.getElementById("result");
+const choices = Array.from(document.querySelectorAll('.click_me'));
+choices.forEach(choice => choice.addEventListener("click", clicked));
 
 
 // const computerSelection = computerPlay();
